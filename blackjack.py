1c948229efa6e3c9541e685021629eb28e1e8e9a
Trevor Hiebert <hieballa@gmail.com>
2012-01-03 21:35:54 -0600

    start on meat of program

--- blackjack.py
+++ blackjack.py
@@ -1,16 +1,18 @@
 print('\n\nHello world!')
 print('Welcome to my game of blackjack!\n\n')
 bank = 10
+play = True
 print('You have: ' + str(bank) + ' credits')
-while play == True:
-    # ask for initial bet (require integer)
+while play:
+    ucont = True
+    wager = input('How much would you like to wager? (Up to three)' : )
     # create deck
     # shuffle deck
     # create user hand
     # create dealer hand (with one card)
     # check for blackjack hand
     # display output
-    while ucont == True:
+    while ucont:
         # whether hit, stay, or double (if first time)
         # display output after each time choosing
         # break if uhand value is over 21
