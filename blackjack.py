faf96304965f0e0240c0d9f77edb66dde263fa02
Trevor Hiebert <hieballa@gmail.com>
2012-01-03 21:00:57 -0600

    program outline

--- blackjack.py
+++ blackjack.py
@@ -1,4 +1,18 @@
 print('\n\nHello world!')
 print('Welcome to my game of blackjack!\n\n')
 bank = 10
+c.value = 0 # total value of cards
 print('You have: ' + str(bank) + ' credits')
+while c.value <= 21
+    # ask for initial bet (require integer)
+    # create deck
+    # create dealer hand
+    # create user hand
+    # ask whether stay, hit, or double
+    # check for blackjack hand
+    # repeat until stay, double, or over 21
+    # random shuffle for computer using dealer rules
+    # evaluation of which is higher, if dealer busts
+    # offer to repeat
+    # evaluation of program-ending score
+    # if bank = 0, program end
