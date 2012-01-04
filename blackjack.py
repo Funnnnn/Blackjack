e518573a40945a7a6abe7e58720200dacfdff784
Trevor Hiebert <hieballa@gmail.com>
2012-01-03 21:13:28 -0600

    mess with the outline

--- blackjack.py
+++ blackjack.py
@@ -2,15 +2,16 @@ print('\n\nHello world!')
 print('Welcome to my game of blackjack!\n\n')
 bank = 10
 print('You have: ' + str(bank) + ' credits')
-while bank > 0
+while bank > 0:
     # ask for initial bet (require integer)
     # create deck
+    # shuffle deck
     # create dealer hand
     # create user hand
-    # ask whether stay, hit, or double
     # check for blackjack hand
+    # ask whether stay, hit, or double
     # repeat until stay, double, or over 21
-    # random shuffle for computer using dealer rules
+    # deal for computer using dealer rules
     # evaluation of which is higher, if dealer busts
     # offer to repeat
     # evaluation of program-ending score
